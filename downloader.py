9d439e1827b237289fdae4dbb9904578e0af491c
Bala_ <balashukla0761@gmail.com>
2023-08-07 20:31:07 -0500

    works with basic inputs

--- downloader.py
+++ downloader.py
@@ -1,1 +1,9 @@
-print("test")
+from pytube import YouTube
+from sys import argv
+
+print("Enter video link: ")
+link = input()
+yt = YouTube(link)
+
+yd = yt.streams.get_highest_resolution()
+yd.download("YoutubeVideoDownloader\downloads") ## enter your file path
