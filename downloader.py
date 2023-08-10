cc3b63b109151ff163186c1eff3b84e73f6a418d
Bala_ <balashukla0761@gmail.com>
2023-08-09 21:41:38 -0500

    tkinter ui

--- downloader.py
+++ downloader.py
@@ -1,9 +1,37 @@
 from pytube import YouTube
-from sys import argv
+import tkinter as tk
+from tkinter import simpledialog
+ 
+window = tk.Tk()
+promptText = tk.Label(
+    text="Enter Youtube Address Below",
+    foreground="white",  
+    background="gray",
+    width=25,
+    height=10
+)
+entry = tk.Entry()
+button = tk.Button(
+    text="Convert Link to MP4",
+    width=25,
+    height=3,
+    bg="blue",
+    fg="yellow",
+)
 
-print("Enter video link: ")
-link = input()
-yt = YouTube(link)
+def click(event):
+    print("The button was clicked!")
+    link = entry.get()
+    window.quit()
+    yt = YouTube(link)
+    yd = yt.streams.get_highest_resolution()
+    yd.download("YoutubeVideoDownloader\downloads") ## enter your file path
 
-yd = yt.streams.get_highest_resolution()
-yd.download("YoutubeVideoDownloader\downloads") ## enter your file path
+
+button.bind("<Button-1>", click)
+
+promptText.pack()
+entry.pack()
+button.pack()
+window.mainloop()
+
